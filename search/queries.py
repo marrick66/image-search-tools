e724e1ca474938faa0e6db7d9f963b802db6e4c3
Sean Mayfield <marrick66@gmail.com>
2023-12-15 14:15:11 -0600

    Quoted exclusion query strings

--- search/queries.py
+++ search/queries.py
@@ -5,7 +5,7 @@ def to_query_str(keywords, exclusions=[], prefix=None):
     query_str = f'{prefix} {keywords}' if prefix != None else keywords
     
     if len(exclusions) > 0:
-        exclude_keywords = [f'-{exclusion}' for exclusion in exclusions]
+        exclude_keywords = [f'-"{exclusion}"' for exclusion in exclusions]
         exclude_str = " ".join(exclude_keywords)
         query_str = f'{query_str} {exclude_str}'
     
